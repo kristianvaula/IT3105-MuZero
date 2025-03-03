63bba573ed566fd2ba633dfa5be7979eed6722b2
Aleksander Olsvik <aleksanderolsvik02@gmail.com>
2025-03-03 13:19:42 +0100

    Add NeuralNetManager class for managing neural network components

---
+++ src/networks/neural_network_manager.py
@@ -0,0 +1,17 @@
+from src.networks.network_builder import NetworkBuilder
+
+class NeuralNetManager():
+    def __init__(self, ):
+        self.representation = None
+        self.dynamics = None
+        self.prediction = None
+        
+    def build_networks(self):
+        builder = NetworkBuilder()
+    
+    def NNr():
+        pass
+    
+    def NNp(self, state):
+        return self.prediction(state)
+    
