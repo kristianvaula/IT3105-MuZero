e68f3dccb6a7bb367c8945a059477024917eb4b9
Kristian Vaula <kristian_vaula@hotmail.com>
2025-04-11 15:26:18 +0200

    Trying atari configs

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import yaml
 from pathlib import Path
 from dataclasses import dataclass
+from typing import List, Optional, Union
 
 @dataclass
 class LoggingConfig:
@@ -11,15 +12,17 @@ class LoggingConfig:
     save_model: bool
     checkpoint_interval: int
 
+
 @dataclass
 class NetworkConfig:
     iteration: str
-    state_window: int
-    roll_ahead: int
-    hidden_state_size: int
-    representation: list
-    dynamics: list
-    prediction: list
+    representation: List[dict]
+    prediction: List[dict]
+    dynamics: List[dict]
+    # Optional for environments like riverraid
+    state_window: Optional[int] = None
+    roll_ahead: Optional[int] = None
+    hidden_state_size: Optional[int] = None
 
 
 @dataclass
@@ -40,9 +43,22 @@ class SnakePacConfig:
     world_length: int
     seed: int
     action_space: int
-    uMCTS: uMCTSConfig
+    mcts: uMCTSConfig
     network: NetworkConfig
 
+
+@dataclass
+class RiverraidConfig:
+    seed: int
+    num_episodes: int
+    num_episode_step: int
+    training_interval: int
+    buffer_size: int
+    batch_size: int
+    skip_frames: int
+    action_frames: int
+    mcts: uMCTSConfig
+    network: NetworkConfig
 
 class Config:
     def __init__(self, config_file: str = "config.yaml"):
@@ -52,13 +68,17 @@ class Config:
         self.logging = LoggingConfig(**self.__config_data["logging_config"])
 
         if self.environment_name == "snakepac":
-            self.environment = SnakePacConfig(**self.__config_data["snakepac"])
-            self.uMCTS = uMCTSConfig(**self.__config_data["snakepac"]["uMCTS"])
-            self.networks = NetworkConfig(**self.__config_data["snakepac"]["network"])
+            env_data = self.__config_data["snakepac"]
+            self.environment = SnakePacConfig(**env_data)
+            self.networks =  NetworkConfig(**env_data["network"])
+            self.uMCTS = uMCTSConfig(**env_data["uMCTS"])
+        elif self.environment_name == "riverraid":
+            env_data = self.__config_data["riverraid"]
+            self.environment = RiverraidConfig(**env_data)
+            self.networks =  NetworkConfig(**env_data["network"])
+            self.uMCTS = uMCTSConfig(**env_data["mcts"])
         else:
-            raise ValueError(
-                f"Invalid environment: {self.environment}"
-            )  # Change when new environments are added
+            raise ValueError(f"Invalid environment: {self.environment_name}")
 
     @staticmethod
     def __load_yaml(config_file: str):
@@ -73,6 +93,5 @@ class Config:
 # Example usage:
 if __name__ == "__main__":
     config = Config()
-
-    print("Environment Configuration:", config.environment)
-    print("Training Configuration:", config.training)
+    print("Environment:", config.environment_name)
+    print("Config:", config.environment)
